e00563912000f879bc13fe0f0a5d2137f61a5495
Mike <55744820+mike-ada@users.noreply.github.com>
2020-11-23 08:48:05 +0000

    Add files via upload

---
+++ class.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class c_ground {
+}
+
+
+class c_crate {
+}
+
+
+class c_fuzzball {
+}
---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+//const { Engine, World, Bodies, Body } = Matter;
+
+var vp_width = 920, vp_height = 690;
+var world, engine, body;
+
+//enable the matter engine
+engine = Matter.Engine.create();
+world = engine.world;
+body = Matter.Body;
+
+
+function apply_velocity() {
+};
+
+function apply_angularvelocity() {
+};
+
+function apply_force() {
+};
+
+
+function get_random(min, max) {
+}
+
+function preload() {
+	//p5 defined function
+}
+
+function setup() {
+}
+
+
+function paint_background() {
+}
+
+
+function paint_assets() {
+}
+
+
+function draw() {
+}
+
+
+
+
